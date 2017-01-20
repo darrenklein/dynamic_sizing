d5304c902e2030f24e08ef8b6e27231fed04d325
Darren Klein <darren.a.klein@gmail.com>
2017-01-20 17:00:45 -0500

    a little cleanup, a few conceptual fixes

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var initialWindowHeight = window.innerHeight;
 var elementWidth = 40;
 var elementHeight = 20;
 var scalingUnit = "vh";
+var squareHTMLCollection = document.getElementsByClassName("red_square");
+var squareObjectCollection = [];
 
 function Square(element, width, height, scalingUnit){
 	this.element = element;
@@ -11,29 +13,28 @@ function Square(element, width, height, scalingUnit){
 	this.scalingUnit = scalingUnit;
 	this.element.style.width = this.width + this.scalingUnit;
 	this.element.style.height = this.height + this.scalingUnit;
+	this.element.style.maxWidth = "286px";
+	this.element.style.maxHeight = "143px";
 };
 
-var squareHTMLCollection = document.getElementsByClassName("red_square");
-var squareObjectCollection = [];
+Square.prototype.calculatePixels = function(windowHeight, elementHeight, elementWidth){
+	return {
+		heightPixels: windowHeight * (elementHeight / 100),
+		widthPixels: windowHeight * (elementWidth / 100)
+	};
+};
+
+Square.prototype.convertScalingUnits = function(windowWidth, elementPixels, newScalingUnit){
+	var newWidth = ((elementPixels.widthPixels / windowWidth) * 100);
+	var newHeight = ((elementPixels.heightPixels / windowWidth) * 100);
+	this.newScalingUnit = newScalingUnit;
+	this.element.style.width = newWidth + this.newScalingUnit;
+	this.element.style.height = newHeight + this.newScalingUnit;
+};
+
 
 for(var i = 0; i < squareHTMLCollection.length; i++){
 	this['square' + i] = new Square(squareHTMLCollection[i], elementWidth, elementHeight, scalingUnit);
-
-	Square.prototype.calculatePixels = function(windowHeight, elementHeight, elementWidth){
-		return {
-			heightPixels: windowHeight * (elementHeight / 100),
-			widthPixels: windowHeight * (elementWidth / 100)
-		};
-	};
-
-	Square.prototype.convertScalingUnits = function(windowWidth, elementPixels, newScalingUnit){
-		var newWidth = ((elementPixels.widthPixels / windowWidth) * 100);
-		var newHeight = ((elementPixels.heightPixels / windowWidth) * 100);
-		this.newScalingUnit = newScalingUnit;
-		this.element.style.width = newWidth + this.newScalingUnit;
-		this.element.style.height = newHeight + this.newScalingUnit;
-	};
-
 	squareObjectCollection.push(this['square' + i]);
 };
 
@@ -41,7 +42,7 @@ window.onresize = function(){
 	var newWindowWidth = window.innerWidth;
 	var newWindowHeight = window.innerHeight;
 
-	if(newWindowWidth <= initialWindowWidth){
+	if(newWindowWidth !== initialWindowWidth){
 		var newScalingUnit = "vw";
 		for(var i = 0; i < squareObjectCollection.length; i++){
 			var initialElementPixels = squareObjectCollection[i].calculatePixels(newWindowHeight, squareObjectCollection[i].height, squareObjectCollection[i].width);
